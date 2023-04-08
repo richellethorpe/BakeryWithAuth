2ab2ee06eb4a39f292b84d7852c06330f47deafb
Richelle <richelle.ak@gmail.com>
2023-04-08 06:21:13 -0700

    add flavor create and details

---
+++ Bakery/Controllers/FlavorController.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bakery.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly BakeryContext _db;
+
+    public CategoriesController(BakeryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
